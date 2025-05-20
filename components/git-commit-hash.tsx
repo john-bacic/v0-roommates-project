b1355ec0d1b2d370e172ffa721f2fe03a12f0747
John Bacic <johnbacic@mac.com>
2025-05-20 09:50:18 -0400

    Update GitCommitHash component to always show local hash instead of 'update available'

--- components/git-commit-hash.tsx
+++ components/git-commit-hash.tsx
@@ -13,35 +13,9 @@ export default function GitCommitHash() {
   useEffect(() => {
     // Set isClient to true when component mounts on the client side
     setIsClient(true);
-
-    // Only try to fetch the latest commit hash in production
-    if (process.env.NODE_ENV === 'production') {
-      // Function to fetch the latest commit hash from GitHub
-      const fetchLatestCommitHash = async () => {
-        try {
-          const response = await fetch('https://api.github.com/repos/john-bacic/v0-roommates-project/commits/clean-start');
-          if (response.ok) {
-            const data = await response.json();
-            // Get the short version of the commit hash (first 7 characters)
-            const shortHash = data.sha.substring(0, 7);
-            if (shortHash !== BUILD_COMMIT_HASH) {
-              setCommitHash(shortHash);
-              setIsLocal(false);
-            }
-          }
-        } catch (error) {
-          console.error('Error fetching latest commit hash:', error);
-        }
-      };
-
-      fetchLatestCommitHash();
-
-      // Set up an interval to refresh the commit hash every hour
-      const intervalId = setInterval(fetchLatestCommitHash, 60 * 60 * 1000);
-      
-      // Clean up the interval when the component unmounts
-      return () => clearInterval(intervalId);
-    }
+    
+    // We no longer need to fetch the latest commit hash
+    // as we always use the local build hash
   }, []);
 
   // Only render the component on the client side
@@ -51,7 +25,7 @@ export default function GitCommitHash() {
         data-component-name="GitCommitHash"
         className="text-[10px] text-[#666666] whitespace-nowrap"
       >
-        build: {BUILD_COMMIT_HASH}
+        build: {BUILD_COMMIT_HASH} (local)
       </span>
     );
   }
@@ -61,7 +35,7 @@ export default function GitCommitHash() {
       data-component-name="GitCommitHash"
       className="text-[10px] text-[#666666] whitespace-nowrap"
     >
-      build: {commitHash} {isLocal ? '(local)' : '(update available)'}
+      build: {BUILD_COMMIT_HASH} (local)
     </span>
   );
 }
