144473ca17399763c6b9f706da78a160435f29b0
John Bacic <johnbacic@mac.com>
2025-06-24 16:20:47 -0400

    Center Messages title in header

--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -90,7 +90,8 @@ function MessagesPage() {
             <button onClick={() => router.back()} className="text-white hover:text-gray-300">
               <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7"></path></svg>
             </button>
-            <h1 className="text-lg font-semibold text-white">Messages</h1>
+            <h1 className="text-lg font-semibold text-white absolute left-1/2 transform -translate-x-1/2">Messages</h1>
+            <div className="w-6"></div>
           </div>
         </div>
 
